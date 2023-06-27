56b98b1b0c04921c39339ec5aa6163945f3e0397
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-06-27 09:10:06 +0300

    assertj, harmcrest

--- src/main/java/com/dmdev/junit/service/UserService.java
+++ src/main/java/com/dmdev/junit/service/UserService.java
@@ -2,9 +2,9 @@ package com.dmdev.junit.service;
 
 import com.dmdev.junit.dto.User;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class UserService {
     private final List<User> users = new ArrayList<>();
@@ -13,8 +13,8 @@ public class UserService {
         return users;
     }
 
-    public boolean add(User user) {
-        return users.add(user);
+    public void add(User... users) {
+        this.users.addAll(Arrays.asList(users));
     }
 
     public Optional<User> login(String username, String password) {
@@ -22,5 +22,10 @@ public class UserService {
                 .filter(user->user.getUsername().equals(username))
                 .filter(user->user.getPassword().equals(password))
                 .findFirst();
+    }
+
+    public Map<Integer, User> getAllConvertedById() {
+        return users.stream()
+                .collect(Collectors.toMap(User::getId, Function.identity()));
     }
 }
--- src/test/java/com/dmdev/junit/service/UserServiceTest.java
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -1,9 +1,15 @@
 package com.dmdev.junit.service;
 
 import com.dmdev.junit.dto.User;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsEmptyCollection;
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.jupiter.api.*;
 
+import java.util.Map;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 // каждый раз объект этого класса пересоздается заново
@@ -32,7 +38,11 @@ public class UserServiceTest {
     void usersEmptyIfNoUsersAdded() {
         System.out.println("Test 1: " + this);
         var users = userService.getAll();
-        Assertions.assertTrue(users.isEmpty(), () -> "User List should be empty");
+
+        //Harmcrest
+        MatcherAssert.assertThat(users, IsEmptyCollection.empty());
+
+        //Assertions.assertTrue(users.isEmpty(), () -> "User List should be empty");
     }
 
     @Test
@@ -42,7 +52,9 @@ public class UserServiceTest {
         userService.add(PETR);
 
         var users = userService.getAll();
-        Assertions.assertEquals(2, users.size());
+        //AssertJ
+        assertThat(users).hasSize(2);
+//        Assertions.assertEquals(2, users.size());
     }
 
     @Test
@@ -51,8 +63,26 @@ public class UserServiceTest {
         userService.add(PETR);
         Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
 
-        Assertions.assertTrue(maybeUser.isPresent());
-        maybeUser.ifPresent(user -> Assertions.assertEquals(IVAN,user));
+        //AssertJ
+        assertThat(maybeUser).isPresent();
+        maybeUser.ifPresent(user->assertThat(user).isEqualTo(IVAN));
+//        Assertions.assertTrue(maybeUser.isPresent());
+//        maybeUser.ifPresent(user -> Assertions.assertEquals(IVAN,user));
+    }
+
+    @Test
+    void usersConvertedToMapById(){
+        userService.add(IVAN, PETR);
+
+        Map<Integer, User> users = userService.getAllConvertedById();
+//        AssertJ
+//        Assertions.assertAll(
+//                ()->assertThat(users).containsKeys(IVAN.getId(), PETR.getId()),
+//                ()->assertThat(users).containsValues(IVAN, PETR)
+//        );
+
+        //Harmcrest
+        MatcherAssert.assertThat(users, IsMapContaining.hasKey(IVAN.getId()));
     }
 
     @Test
