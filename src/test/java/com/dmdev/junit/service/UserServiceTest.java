438ae5b488a9a71c3b5c1602dee7f2006edc1517
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-06-27 08:42:51 +0300

    tdd

--- src/test/java/com/dmdev/junit/service/UserServiceTest.java
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -3,6 +3,8 @@ package com.dmdev.junit.service;
 import com.dmdev.junit.dto.User;
 import org.junit.jupiter.api.*;
 
+import java.util.Optional;
+
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 // каждый раз объект этого класса пересоздается заново
 // для каждого теста
@@ -11,6 +13,8 @@ import org.junit.jupiter.api.*;
 // т.к. уже есть конкретный объект
 public class UserServiceTest {
 
+    private static final User IVAN = User.of(1, "Ivan", "123");
+    private static final User PETR = User.of(2, "Petr", "111");
     private UserService userService;
 
     @BeforeAll
@@ -34,11 +38,37 @@ public class UserServiceTest {
     @Test
     void userSizeIfUserAdded(){
         System.out.println("Test 2: " + this);
-        userService.add(new User());
-        userService.add(new User());
+        userService.add(IVAN);
+        userService.add(PETR);
 
         var users = userService.getAll();
         Assertions.assertEquals(2, users.size());
+    }
+
+    @Test
+    void loginSuccessIfUserExists(){
+        userService.add(IVAN);
+        userService.add(PETR);
+        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+        Assertions.assertTrue(maybeUser.isPresent());
+        maybeUser.ifPresent(user -> Assertions.assertEquals(IVAN,user));
+    }
+
+    @Test
+    void loginFailIfPasswordIsNotCorrect(){
+        userService.add(IVAN);
+        var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+
+        Assertions.assertTrue(maybeUser.isEmpty());
+    }
+
+    @Test
+    void loginFailIfUserDoesNotExist(){
+        userService.add(IVAN);
+        var maybeUser = userService.login("dummy", IVAN.getPassword());
+
+        Assertions.assertTrue(maybeUser.isEmpty());
     }
 
     @AfterEach
