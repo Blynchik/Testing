728b7ed5598e70bfda8f7342cbfee33925510d9a
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-06-28 12:59:06 +0300

    nested tests and order

--- src/test/java/com/dmdev/junit/service/UserServiceTest.java
+++ src/test/java/com/dmdev/junit/service/UserServiceTest.java
@@ -25,6 +25,10 @@ import static org.assertj.core.api.Assertions.fail;
 //или командную строку например
 //mvn clean test -DexcludedGroups=login
 //mvn clean test -Dgroups=login
+//@TestMethodOrder(MethodOrderer.OrderAnnotation.class) - если над тестом задан @Order
+//то тест выполнится в заданном порядке, значения могут быть не последовательными
+//@TestMethodOrder(MethodOrderer.MethodName.class) - в алфавитном порядке
+//@TestMethodOrder(MethodOrderer.DisplayName.class) - в алфавитном порядке названия тестов в отображении с аннотацией @DisplayName
 public class UserServiceTest {
 
     private static final User IVAN = User.of(1, "Ivan", "123");
@@ -43,6 +47,7 @@ public class UserServiceTest {
     }
 
     @Test
+//    @Order(1)
     void usersEmptyIfNoUsersAdded() {
         System.out.println("Test 1: " + this);
         var users = userService.getAll();
@@ -54,6 +59,7 @@ public class UserServiceTest {
     }
 
     @Test
+//    @Order(2)
     void userSizeIfUserAdded() {
         System.out.println("Test 2: " + this);
         userService.add(IVAN);
@@ -63,32 +69,6 @@ public class UserServiceTest {
         //AssertJ
         assertThat(users).hasSize(2);
 //        Assertions.assertEquals(2, users.size());
-    }
-
-    @Test
-    @Tag("login")
-    void loginSuccessIfUserExists() {
-        userService.add(IVAN);
-        userService.add(PETR);
-        Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
-
-        //AssertJ
-        assertThat(maybeUser).isPresent();
-        maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
-//        Assertions.assertTrue(maybeUser.isPresent());
-//        maybeUser.ifPresent(user -> Assertions.assertEquals(IVAN,user));
-    }
-
-    @Test
-    @Tag("login")
-    void throwExceptionIfUsernameOrPasswordIsNull() {
-        Assertions.assertAll(
-                () -> {
-                    var exception = Assertions.assertThrows(IllegalArgumentException.class, () -> userService.login(null, "dummy"));
-                    assertThat(exception.getMessage()).isEqualTo("username or password is null");
-                },
-                () -> Assertions.assertThrows(IllegalArgumentException.class, () -> userService.login("dummy", null))
-        );
     }
 
     @Test
@@ -132,5 +112,56 @@ public class UserServiceTest {
     @AfterAll
     static void closeConnectionPool() {
         System.out.println("After all: ");
+    }
+
+    //можно создать внутренний класс, где будут выполняться схожие тесты
+    //можно вест класс сделать @Tag("login"), все методы будут с этим тегом
+    @Nested
+    //Сообщает, что внутри класса есть тесты, которые нужно выполнить
+            // @DisplayName тоже можно
+    class LoginTest{
+        @Test
+        @Tag("login")
+        void loginSuccessIfUserExists() {
+            userService.add(IVAN);
+            userService.add(PETR);
+            Optional<User> maybeUser = userService.login(IVAN.getUsername(), IVAN.getPassword());
+
+            //AssertJ
+            assertThat(maybeUser).isPresent();
+            maybeUser.ifPresent(user -> assertThat(user).isEqualTo(IVAN));
+//        Assertions.assertTrue(maybeUser.isPresent());
+//        maybeUser.ifPresent(user -> Assertions.assertEquals(IVAN,user));
+        }
+
+        @Test
+        @Tag("login")
+        void throwExceptionIfUsernameOrPasswordIsNull() {
+            Assertions.assertAll(
+                    () -> {
+                        var exception = Assertions.assertThrows(IllegalArgumentException.class, () -> userService.login(null, "dummy"));
+                        assertThat(exception.getMessage()).isEqualTo("username or password is null");
+                    },
+                    () -> Assertions.assertThrows(IllegalArgumentException.class, () -> userService.login("dummy", null))
+            );
+        }
+
+        @Test
+        @Tag("login")
+        void loginFailIfPasswordIsNotCorrect() {
+            userService.add(IVAN);
+            var maybeUser = userService.login(IVAN.getUsername(), "dummy");
+
+            Assertions.assertTrue(maybeUser.isEmpty());
+        }
+
+        @Test
+        @Tag("login")
+        void loginFailIfUserDoesNotExist() {
+            userService.add(IVAN);
+            var maybeUser = userService.login("dummy", IVAN.getPassword());
+
+            Assertions.assertTrue(maybeUser.isEmpty());
+        }
     }
 }
